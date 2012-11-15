efeadf02c120aa82ba6fa25c73db7ead2d4a87de
XROW System Account <xrow@test01.hande.c.de.siteprotect.eu>
2012-11-15 14:05:32 +0100

    - sed command to cronjob added

--- cronjobs/archivepiwik.php
+++ cronjobs/archivepiwik.php
@@ -3,6 +3,7 @@ echo "Archiveing Piwik-Reports\n";
 system("sudo rm -Rf {$_SERVER['PWD']}/extension/xrowpiwik/src/piwik/tmp/;
 		mkdir {$_SERVER['PWD']}/extension/xrowpiwik/src/piwik/tmp/;
 		chmod -R 777 {$_SERVER['PWD']}/extension/xrowpiwik/src/piwik/tmp/;
+		sed -i 's/TEST_PHP_BIN in php5 php php-cli php-cgi/TEST_PHP_BIN in php5 php php-cli php-cgi \/usr\/local\/zend\/bin\/php/g' {$_SERVER['PWD']}/extension/xrowpiwik/src/piwik/misc/cron/archive.sh;
 		sh extension/xrowpiwik/src/piwik/misc/cron/archive.sh url={$_SERVER['HOSTNAME']}extension/xrowpiwik/src/piwik;
 		sudo rm -Rf {$_SERVER['PWD']}/extension/xrowpiwik/src/piwik/tmp/");
 echo "Archiveing Piwik-Reports DONE!\n";
