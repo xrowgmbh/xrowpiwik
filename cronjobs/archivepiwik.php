3c263dcde81583a4cda3ee272bb417ee8d884d56
Robert <robert@xrow.de>
2016-03-30 13:09:33 +0200

    removed static tmp deleting to fix the permission problems

--- cronjobs/archivepiwik.php
+++ cronjobs/archivepiwik.php
@@ -1,6 +1,5 @@
 <?php 
 echo "Archiveing Piwik-Reports\n";
 system("sed -i 's/TEST_PHP_BIN in php5 php php-cli php-cgi/TEST_PHP_BIN in php5 php php-cli php-cgi \/usr\/local\/zend\/bin\/php/g' {$_SERVER['PWD']}/extension/xrowpiwik/src/piwik/misc/cron/archive.sh;
-		sh extension/xrowpiwik/src/piwik/misc/cron/archive.sh url={$_SERVER['HOSTNAME']}extension/xrowpiwik/src/piwik;
-		sudo rm -Rf {$_SERVER['PWD']}/extension/xrowpiwik/src/piwik/tmp/");
+        sh extension/xrowpiwik/src/piwik/misc/cron/archive.sh url={$_SERVER['HOSTNAME']}extension/xrowpiwik/src/piwik;");
 echo "Archiveing Piwik-Reports DONE!\n";
