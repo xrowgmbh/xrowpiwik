afe14c70ba63ddc89af6aec803a832920145d955
xrowkristina <kristina@xrow.de>
2016-04-11 13:26:41 +0200

    set path to extension to ezpublish_legacy/... and changed mode

--- cronjobs/archivepiwik.php
+++ cronjobs/archivepiwik.php
@@ -1,5 +1,7 @@
 <?php 
 echo "Archiveing Piwik-Reports\n";
-system("sed -i 's/TEST_PHP_BIN in php5 php php-cli php-cgi/TEST_PHP_BIN in php5 php php-cli php-cgi \/usr\/local\/zend\/bin\/php/g' {$_SERVER['PWD']}/extension/xrowpiwik/src/piwik/misc/cron/archive.sh;
+echo "CHMOD -Rf 777 {$_SERVER['PWD']}/ezpublish_legacy/extension/xrowpiwik/src/piwik/tmp/climulti\n";
+system("sudo chmod -Rf 777 {$_SERVER['PWD']}/ezpublish_legacy/extension/xrowpiwik/src/piwik/tmp/climulti");
+system("sed -i 's/TEST_PHP_BIN in php5 php php-cli php-cgi/TEST_PHP_BIN in php5 php php-cli php-cgi \/usr\/local\/zend\/bin\/php/g' {$_SERVER['PWD']}/ezpublish_legacy/extension/xrowpiwik/src/piwik/misc/cron/archive.sh;
         sh extension/xrowpiwik/src/piwik/misc/cron/archive.sh url={$_SERVER['HOSTNAME']}extension/xrowpiwik/src/piwik;");
 echo "Archiveing Piwik-Reports DONE!\n";
