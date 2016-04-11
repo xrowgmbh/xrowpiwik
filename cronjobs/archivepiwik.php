3531d69dc72acccd57ff339ee4c9d200fd136078
xrowkristina <kristina@xrow.de>
2016-04-11 14:42:58 +0200

    Update archivepiwik.php

--- cronjobs/archivepiwik.php
+++ cronjobs/archivepiwik.php
@@ -1,7 +1,7 @@
 <?php 
 echo "Archiveing Piwik-Reports\n";
 echo "CHMOD -Rf 777 {$_SERVER['PWD']}/ezpublish_legacy/extension/xrowpiwik/src/piwik/tmp/climulti\n";
-system("sudo chmod -Rf 777 {$_SERVER['PWD']}/ezpublish_legacy/extension/xrowpiwik/src/piwik/tmp/climulti");
+system("chmod -Rf 777 {$_SERVER['PWD']}/ezpublish_legacy/extension/xrowpiwik/src/piwik/tmp/climulti");
 system("sed -i 's/TEST_PHP_BIN in php5 php php-cli php-cgi/TEST_PHP_BIN in php5 php php-cli php-cgi \/usr\/local\/zend\/bin\/php/g' {$_SERVER['PWD']}/ezpublish_legacy/extension/xrowpiwik/src/piwik/misc/cron/archive.sh;
         sh extension/xrowpiwik/src/piwik/misc/cron/archive.sh url={$_SERVER['HOSTNAME']}extension/xrowpiwik/src/piwik;");
 echo "Archiveing Piwik-Reports DONE!\n";
