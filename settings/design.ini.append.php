b391f1ba71ddf71ac243677d6dbc886c7e10b85e
Björn Dieding <xrow@users.noreply.github.com>
2016-04-14 11:59:06 +0200

    Update design.ini.append.php
    Avoid warning.

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -6,6 +6,6 @@ DesignExtensions[]=xrowpiwik
 [JavaScriptSettings]
 FrontendJavaScriptList[]=xrowpiwik::doPiwikTrack
 #this file does not exists in this position but will be symlinked from extension/xrowpiwik/src/piwik/plugins/ClickHeat/libs/js/clickheat.js
-FrontendJavaScriptList[]=clickheat.js
+#FrontendJavaScriptList[]=clickheat.js
 
 */ ?>
